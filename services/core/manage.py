c75621a213eb3dfda19a5bf3e95a608898c3e489
Ravi <ravichaudhary@zinnov.com>
2019-11-03 15:03:16 +0530

    prod setup

--- services/core/manage.py
+++ services/core/manage.py
@@ -59,5 +59,23 @@ def recreate_db():
     db.session.commit()
 
 
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database."""
+    db.session.add(Products(
+        name=('Dew Of The Sea Hairoil - 100ml'),
+        sku='18729769697378'
+    ))
+    db.session.add(Products(
+        name=('Lin Dior Hairoil - 100ml'),
+        sku='20439188308066'
+    ))
+    db.session.add(Products(
+        name=('Combination Kit 2'),
+        sku='18377561276514'
+    ))
+    db.session.commit()
+
+
 if __name__ == '__main__':
     cli()

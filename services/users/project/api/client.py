c684c64644eb140b1999de8da262ef8519627be3
Arpit <arpitkoolwal855@gmail.com>
2020-09-10 01:42:58 +0530

    Added patch api for client and query params in getclient api

--- services/users/project/api/client.py
+++ services/users/project/api/client.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api
 
 from project import db
 from project.api.models import Client
-from sqlalchemy import exc
+from sqlalchemy import exc, or_
 from project.api.utils import authenticate_restful, is_admin, pagination_validator
 import logging
 
@@ -14,16 +14,16 @@ api = Api(clients_blueprint)
 
 
 class Clients(Resource):
-    method_decorators = {'post': [authenticate_restful], 'get': [authenticate_restful]}
+    method_decorators = {'post': [authenticate_restful], 'get': [authenticate_restful], 'patch': [authenticate_restful]}
 
     def post(self, resp):
         response_object = {'status': 'fail'}
-        post_data = request.get_json()
-        client_name = post_data.get('client_name')
-        client_prefix = post_data.get('client_prefix')
-        primary_email = post_data.get('primary_email')
-        tabs = post_data.get('tabs')
         try:
+            post_data = request.get_json()
+            client_name = post_data.get('client_name')
+            client_prefix = post_data.get('client_prefix')
+            primary_email = post_data.get('primary_email')
+            tabs = post_data.get('tabs')
             client = Client(client_name=client_name, client_prefix=client_prefix, primary_email=primary_email, tabs=tabs)
             db.session.add(client)
             db.session.commit()
@@ -36,12 +36,40 @@ class Clients(Resource):
             response_object['message'] = 'failed while creating client'
             return response_object, 400
 
+    def patch(self, resp):
+        response_object = {'status': 'fail'}
+        try:
+            post_data = request.get_json()
+            client_name = post_data.get('client_name')
+            client_prefix = post_data.get('client_prefix')
+            primary_email = post_data.get('primary_email')
+            tabs = post_data.get('tabs')
+            client = Client.query.filter_by(client_prefix=client_prefix).first()
+            client.client_name = client_name
+            client.primary_email = primary_email
+            client.tabs = tabs
+            db.session.commit()
+            response_object['status'] = 'success'
+            logger.info('client created successfully')
+            return response_object, 200
+        except Exception as e:
+            db.session.rollback()
+            logger.error('Failed while creating the client', e)
+            response_object['message'] = 'failed while creating client'
+            return response_object, 400
+
     def get(self, resp):
+        response_object = {'status': 'fail'}
         try:
             page_number = request.args.get('page_number')
             page_size = request.args.get('page_size')
+            searched_query = request.args.get('search_query')
             page_size, page_number = pagination_validator(page_size, page_number)
-            clients_data = Client.query.paginate(page=page_number, per_page=page_size, error_out=False)
+            clients_data = Client.query.filter(or_(
+                    Client.client_name.ilike(r"%{}%".format(searched_query)),
+                    Client.client_prefix.ilike(r"%{}%".format(searched_query)),
+                    Client.primary_email.ilike(r"%{}%".format(searched_query))
+                   )).paginate(page=page_number, per_page=page_size, error_out=False)
             total_page = clients_data.total // page_size if clients_data.total % page_size == 0 else (clients_data.total // page_size) + 1
             response_object = {
                 'status': 'success',
@@ -56,7 +84,8 @@ class Clients(Resource):
             return response_object, 200
         except Exception as e:
             logger.error('Failed while fetching the client', e)
-            return {'status': 'fail', 'message': 'failed while fetching the client'}, 400
+            response_object['message'] = 'failed while fetching the client'
+            return response_object, 400
 
 
 @clients_blueprint.route('/users/v1/clients/updateStatus', methods=['POST'])

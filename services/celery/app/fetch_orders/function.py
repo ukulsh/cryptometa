8f4a2fafd562c67a9893feef9e627db560d40e05
Ravi Chaudhary <ravi@Ravis-MacBook-Air.local>
2021-02-04 12:17:13 +0530

    flipkartsmart

--- services/celery/app/fetch_orders/function.py
+++ services/celery/app/fetch_orders/function.py
@@ -1314,6 +1314,7 @@ def update_thirdwatch_data(cur):
 
 easyecom_wareiq_channel_map = {"Amazon.in": 2,
                            "Shopify": 1,
+                           "FlipkartSmart": 3,
                                "Flipkart":3}
 
 easyecom_wareiq_courier_map = {"eKart": 7}

db5085821178fc7451dc4b100262ad99bafdfddd
Ravi Chaudhary <ravi@MacBook-Pro.local>
2021-08-04 09:48:24 +0530

    Revert "Merge pull request #24 from ravi-xo/tracking_customization"
    This reverts commit a10a8e6140b00eacb46652b73bd3b7e646e6594d, reversing
    changes made to 9dc9e3a50e876138724e84974de775a0ec91b153.

--- services/celery/app/update_status/update_status_utils.py
+++ services/celery/app/update_status/update_status_utils.py
@@ -8,29 +8,16 @@ from .order_shipped import order_shipped
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-RAVEN_URL = (
-    "https://api.ravenapp.dev/v1/apps/ccaaf889-232e-49df-aeb8-869e3153509d/events/send"
-)
-RAVEN_HEADERS = {
-    "Content-Type": "application/json",
-    "Authorization": "AuthKey K4noY3GgzaW8OEedfZWAOyg+AmKZTsqO/h/8Y4LVtFA=",
-}
+RAVEN_URL = "https://api.ravenapp.dev/v1/apps/ccaaf889-232e-49df-aeb8-869e3153509d/events/send"
+RAVEN_HEADERS = {"Content-Type": "application/json", "Authorization": "AuthKey K4noY3GgzaW8OEedfZWAOyg+AmKZTsqO/h/8Y4LVtFA="}
 
-email_client = boto3.client(
-    "ses",
-    region_name="us-east-1",
-    aws_access_key_id="AKIAWRT2R3KC3YZUBFXY",
-    aws_secret_access_key="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",
-)
+email_client = boto3.client('ses', region_name="us-east-1", aws_access_key_id='AKIAWRT2R3KC3YZUBFXY',
+    aws_secret_access_key='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')
 
 
 def send_shipped_event(mobile, email, order, edd, courier_name, tracking_link=None):
     background_color = str(order[24]) if order[24] else "#B5D0EC"
-    client_logo = (
-        str(order[21])
-        if order[21]
-        else "https://logourls.s3.amazonaws.com/client_logos/logo_ane.png"
-    )
+    client_logo = str(order[21]) if order[21] else "https://logourls.s3.amazonaws.com/client_logos/logo_ane.png"
     client_name = str(order[20]) if order[20] else "WareIQ"
     email_title = str(order[22]) if order[22] else "Your order has been shipped!"
     order_id = str(order[12]) if order[12] else ""
@@ -43,7 +30,10 @@ def send_shipped_event(mobile, email, order, edd, courier_name, tracking_link=No
 
     payload = {
         "event": "shipped",
-        "user": {"mobile": mobile, "email": email if email else ""},
+        "user": {
+            "mobile": mobile,
+            "email": email if email else ""
+        },
         "data": {
             "client_name": client_name,
             "customer_name": customer_name,
@@ -54,9 +44,15 @@ def send_shipped_event(mobile, email, order, edd, courier_name, tracking_link=No
             "edd": edd,
             "awb_number": awb_number,
             "background_color": background_color,
-            "client_logo": client_logo,
+            "client_logo": client_logo
         },
-        "override": {"email": {"from": {"name": client_name}}},
+        "override": {
+            "email": {
+                "from": {
+                    "name": client_name
+                }
+            }
+        }
     }
 
     req = requests.post(RAVEN_URL, headers=RAVEN_HEADERS, data=json.dumps(payload))
@@ -74,8 +70,8 @@ def send_delivered_event(mobile, order, courier_name, tracking_link=None):
         "data": {
             "client_name": client_name,
             "courier_name": courier_name,
-            "tracking_link": tracking_link,
-        },
+            "tracking_link": tracking_link
+        }
     }
 
     req = requests.post(RAVEN_URL, headers=RAVEN_HEADERS, data=json.dumps(payload))
@@ -93,8 +89,8 @@ def send_picked_rvp_event(mobile, order, courier_name, tracking_link=None):
         "data": {
             "client_name": client_name,
             "courier_name": courier_name,
-            "tracking_link": tracking_link,
-        },
+            "tracking_link": tracking_link
+        }
     }
 
     req = requests.post(RAVEN_URL, headers=RAVEN_HEADERS, data=json.dumps(payload))
@@ -112,8 +108,8 @@ def send_delivered_rvp_event(mobile, order, courier_name, tracking_link=None):
         "data": {
             "client_name": client_name,
             "courier_name": courier_name,
-            "tracking_link": tracking_link,
-        },
+            "tracking_link": tracking_link
+        }
     }
 
     req = requests.post(RAVEN_URL, headers=RAVEN_HEADERS, data=json.dumps(payload))
@@ -129,54 +125,40 @@ def send_ndr_event(mobile, order, verification_link):
         "data": {
             "client_name": client_name,
             "verification_link": verification_link,
-        },
+        }
     }
 
     req = requests.post(RAVEN_URL, headers=RAVEN_HEADERS, data=json.dumps(payload))
 
 
 def send_bulk_emails(emails):
-    logger.info(
-        "Sending Emails....count: "
-        + str(len(emails))
-        + "  Time: "
-        + str(datetime.utcnow())
-    )
+    logger.info("Sending Emails....count: " + str(len(emails)) + "  Time: " + str(datetime.utcnow()))
     for email in emails:
         try:
             response = email_client.send_raw_email(
-                Source=email[0]["From"],
+                Source=email[0]['From'],
                 Destinations=email[1],
                 RawMessage={
-                    "Data": email[0].as_string(),
+                    'Data': email[0].as_string(),
                 },
             )
             sleep(0.08)
         except Exception as e:
-            logger.error(
-                "Couldn't send email: "
-                + str(email["TO"])
-                + "\nError: "
-                + str(e.args[0])
-            )
+            logger.error("Couldn't send email: " + str(email['TO'])+"\nError: "+str(e.args[0]))
 
 
 def create_email(order, edd, email):
     try:
         background_color = str(order[24]) if order[24] else "#B5D0EC"
-        client_logo = (
-            str(order[21])
-            if order[21]
-            else "https://logourls.s3.amazonaws.com/client_logos/logo_ane.png"
-        )
+        client_logo = str(order[21]) if order[21] else "https://logourls.s3.amazonaws.com/client_logos/logo_ane.png"
         client_name = str(order[20]) if order[20] else "WareIQ"
         email_title = str(order[22]) if order[22] else "Your order has been shipped!"
         order_id = str(order[12]) if order[12] else ""
         customer_name = str(order[18]) if order[18] else "Customer"
         courier_name = "WareIQ"
-        if order[23] in (1, 2, 8, 11, 12):
+        if order[23] in (1,2,8,11,12):
             courier_name = "Delhivery"
-        elif order[23] in (5, 13):
+        elif order[23] in (5,13):
             courier_name = "Xpressbees"
         elif order[23] in (4,):
             courier_name = "Shadowfax"
@@ -187,26 +169,23 @@ def create_email(order, edd, email):
         awb_number = str(order[1]) if order[1] else ""
         tracking_link = "http://webapp.wareiq.com/tracking/" + str(order[1])
 
-        html = (
-            order_shipped.replace("__CLIENT_LOGO__", client_logo)
-            .replace("__CLIENT_NAME__", client_name)
-            .replace("__BACKGROUND_COLOR__", background_color)
-            .replace("__EMAIL_TITLE__", email_title)
-            .replace("__CUSTOMER_NAME__", customer_name)
-            .replace("__ORDER_ID__", order_id)
-            .replace("__COURIER_NAME__", courier_name)
-            .replace("__EDD__", edd)
-            .replace("__AWB_NUMBER__", awb_number)
-            .replace("__TRACKING_LINK__", tracking_link)
-        )
+        html = order_shipped.replace('__CLIENT_LOGO__', client_logo)\
+            .replace('__CLIENT_NAME__',  client_name)\
+            .replace('__BACKGROUND_COLOR__', background_color)\
+            .replace('__EMAIL_TITLE__', email_title)\
+            .replace('__CUSTOMER_NAME__', customer_name)\
+            .replace('__ORDER_ID__', order_id)\
+            .replace('__COURIER_NAME__', courier_name)\
+            .replace('__EDD__', edd)\
+            .replace('__AWB_NUMBER__', awb_number).replace('__TRACKING_LINK__', tracking_link)
 
         # create message object instance
-        msg = MIMEMultipart("alternative")
+        msg = MIMEMultipart('alternative')
 
         recipients = [email]
-        msg["From"] = "%s <noreply@wareiq.com>" % client_name
-        msg["To"] = ", ".join(recipients)
-        msg["Subject"] = email_title
+        msg['From'] = "%s <noreply@wareiq.com>"%client_name
+        msg['To'] = ", ".join(recipients)
+        msg['Subject'] = email_title
 
         # write the HTML part
 
@@ -214,53 +193,36 @@ def create_email(order, edd, email):
         msg.attach(part2)
         return msg
     except Exception as e:
-        logger.error(
-            "Couldn't send email: " + str(order[1]) + "\nError: " + str(e.args)
-        )
+        logger.error("Couldn't send email: " + str(order[1]) + "\nError: " + str(e.args))
         return None
 
 
-def webhook_updates(
-    order, cur, status, status_text, location, status_time, ndr_id=None
-):
+def webhook_updates(order, cur, status, status_text, location, status_time, ndr_id=None):
     if order[38]:
         try:
             if ndr_id:
-                cur.execute("SELECT reason FROM ndr_reasons WHERE id=%s" % str(ndr_id))
+                cur.execute("SELECT reason FROM ndr_reasons WHERE id=%s"%str(ndr_id))
                 status_text = cur.fetchone()[0]
-            cur.execute(
-                "SELECT webhook_url, header_key, header_value, webhook_secret, id FROM webhooks WHERE status='active' and client_prefix='%s'"
-                % order[3]
-            )
+            cur.execute("SELECT webhook_url, header_key, header_value, webhook_secret, id FROM webhooks WHERE status='active' and client_prefix='%s'"%order[3])
             all_webhooks = cur.fetchall()
             for webhook in all_webhooks:
                 try:
-                    req_body = {
-                        "awb": order[1],
-                        "status": status,
-                        "event_time": status_time,
-                        "location": location,
-                        "order_id": order[12],
-                        "status_text": status_text,
-                    }
+                    req_body = {"awb": order[1],
+                                "status": status,
+                                "event_time": status_time,
+                                "location": location,
+                                "order_id": order[12],
+                                "status_text": status_text}
 
                     headers = {"Content-Type": "application/json"}
                     if webhook[1] and webhook[2]:
                         headers[webhook[1]] = webhook[2]
 
-                    req = requests.post(
-                        webhook[0], headers=headers, json=req_body, timeout=5
-                    )
-                    if not str(req.status_code).startswith("2"):
-                        cur.execute(
-                            "UPDATE webhooks SET fail_count=fail_count+1 WHERE id=%s"
-                            % str(webhook[4])
-                        )
+                    req = requests.post(webhook[0], headers=headers, json=req_body, timeout=5)
+                    if not str(req.status_code).startswith('2'):
+                        cur.execute("UPDATE webhooks SET fail_count=fail_count+1 WHERE id=%s" % str(webhook[4]))
                 except Exception:
-                    cur.execute(
-                        "UPDATE webhooks SET fail_count=fail_count+1 WHERE id=%s"
-                        % str(webhook[4])
-                    )
+                    cur.execute("UPDATE webhooks SET fail_count=fail_count+1 WHERE id=%s"%str(webhook[4]))
                     pass
         except Exception:
             pass
